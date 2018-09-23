880531f402bb7c2df55b9c9d3358ed2d7952b4fd
jebaxx <jxxebata@gmail.com>
2018-09-23 12:44:24 +0900

    first commit

---
+++ monitorBase.py
@@ -0,0 +1,480 @@
+#!/usr/bin/python
+#
+import os
+import errno
+import logging
+import time
+import RPi.GPIO as GPIO
+import threading
+import smbus
+from datetime import datetime
+import requests
+import math
+
+__i2c = None
+
+######################################################################
+#  MAIN process
+#
+#	Read senser data periodically and POST to a GAE server
+#	and the local file (for local UI)
+#====================================================================#
+
+#====================================================================#
+#  3 color led controller class
+#====================================================================#
+
+class c3_m:
+
+    _i2c = None
+    _t1  = 0
+    _t2r = 0
+    _t2g = 0
+    _t2b = 0
+    _dt  = 1/4.0
+
+    #- - - - - - - - - - - - - - - - - -
+    # LED controller constant
+
+    i2cAddr_BTN = 0x3f
+
+    REG_INPUT    = 0x00
+    REG_OUTPUT   = 0x01
+    REG_POLARITY = 0x02
+    REG_CONFIG   = 0x03
+
+    LED_DATA = 7
+    LED_CLOCK = 6
+
+    LED_GAMMA = [
+	  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+	  0,   0,   0,   0,   0,   0,   1,   1,   1,   1,   1,   1,   1,   2,   2,   2,
+	  2,   2,   2,   3,   3,   3,   3,   3,   4,   4,   4,   4,   5,   5,   5,   5,
+	  6,   6,   6,   7,   7,   7,   8,   8,   8,   9,   9,   9,  10,  10,  11,  11,
+	 11,  12,  12,  13,  13,  13,  14,  14,  15,  15,  16,  16,  17,  17,  18,  18,
+	 19,  19,  20,  21,  21,  22,  22 , 23,  23,  24,  25,  25,  26,  27,  27,  28,
+	 29,  29,  30,  31,  31,  32,  33,  34,  34,  35,  36,  37,  37,  38,  39,  40,
+	 40,  41,  42,  43,  44,  45,  46,  46,  47,  48,  49,  50,  51,  52,  53,  54,
+	 55,  56,  57,  58,  59,  60,  61,  62,  63,  64,  65,  66,  67,  68,  69,  70,
+	 71,  72,  73,  74,  76,  77,  78,  79,  80,  81,  83,  84,  85,  86,  88,  89,
+	 90,  91,  93,  94,  95,  96,  98,  99, 100, 102, 103, 104, 106, 107, 109, 110,
+	111, 113, 114, 116, 117, 119, 120, 121, 123, 124, 126, 128, 129, 131, 132, 134,
+	135, 137, 138, 140, 142, 143, 145, 146, 148, 150, 151, 153, 155, 157, 158, 160,
+	162, 163, 165, 167, 169, 170, 172, 174, 176, 178, 179, 181, 183, 185, 187, 189,
+	191, 193, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220,
+	222, 224, 227, 229, 231, 233, 235, 237, 239, 241, 244, 246, 248, 250, 252, 255]
+
+    _brightness = 0.8
+
+    @staticmethod
+    def init(__i2c):
+	c3_m._i2c = __i2c
+
+	c3_m._i2c.write_byte_data(c3_m.i2cAddr_BTN, c3_m.REG_CONFIG, 0b00011111)
+	c3_m._i2c.write_byte_data(c3_m.i2cAddr_BTN, c3_m.REG_POLARITY, 0b00000000)
+	c3_m._i2c.write_byte_data(c3_m.i2cAddr_BTN, c3_m.REG_OUTPUT, 0b00000000)
+
+	c3_m.set_pixel(0, 100, 200)
+
+    @staticmethod
+    def _write_byte(byte):
+	cmd = []
+	for x in range(8):
+	    if (byte & 0b10000000) : _cmd = (1 << c3_m.LED_DATA)
+	    else:			 _cmd = 0
+	    cmd.append(_cmd)
+	    _cmd |= (1 << c3_m.LED_CLOCK)
+	    cmd.append(_cmd)
+	    byte <<= 1
+
+	c3_m._i2c.write_i2c_block_data(c3_m.i2cAddr_BTN, c3_m.REG_OUTPUT, cmd)
+
+    @staticmethod
+    def set_pixel(r, g, b):
+	r, g, b = [int(x * c3_m._brightness) for x in (r, g, b)]
+
+	c3_m._write_byte(0)
+	c3_m._write_byte(0)
+	c3_m._write_byte(0b11101111)
+	c3_m._write_byte(c3_m.LED_GAMMA[b & 0xff])
+	c3_m._write_byte(c3_m.LED_GAMMA[g & 0xff])
+	c3_m._write_byte(c3_m.LED_GAMMA[r & 0xff])
+	c3_m._write_byte(0)
+	c3_m._write_byte(0)
+
+    @staticmethod
+    def polling():
+
+	c3_m._t1 += c3_m._dt
+	mtr = abs((c3_m._t1 % 80) - 40) + 10.0
+	mtg = abs((c3_m._t1 % 82) - 41) + 10.1
+	mtb = abs((c3_m._t1 % 84) - 42) + 9.9
+	
+	c3_m._t2r += c3_m._dt / mtr
+	c3_m._t2g += c3_m._dt / mtg
+	c3_m._t2b += c3_m._dt / mtb
+
+	Lr = math.sin(c3_m._t2r * math.pi) * 180 + 60
+	if Lr < 0 : Lr = 0
+	Lg = math.sin(c3_m._t2g * math.pi) * 180 + 60
+	if Lg < 0 : Lg = 0
+	Lb = math.sin(c3_m._t2b * math.pi) * 180 + 60
+	if Lb < 0 : Lb = 0
+	c3_m.set_pixel(Lr, Lg, Lb)
+
+#====================================================================#
+#  LED ON / OFF  controller class
+#====================================================================#
+
+class ld():
+
+    i2cAddr_LD = 0x3c	# SO1602 LED display I2C Address
+    _i2c = None
+
+    #---------------------------------------------------------------------------------
+    N   = 8		# 1   # 1: 2/4-line mode  0: 1/3-line mode
+    DH  = 4		# 0/1 # Double height font control for 2-line mode
+    RE  = 2		# ... # RE flag
+    IS  = 1		# ... # IS flag
+    BE  = 4		# 0   # CGRAM blink 1: Enable  0: Disable
+    REV = 1		# 0   # 1: Reverse display  0: Normal display
+
+    FW  = 4		# 0   # 1: 6-dot font  0: 5-dot font
+    BW  = 2		# 0   # inverting cursor  1: Enable 0: disable
+    NW  = 1		# 0   # 1: 3 or 4 line  0: 1 or 2 line
+    #---------------------------------------------------------------------------------
+    UD2 = 8		# 1   # Double height format (should be 1 )
+    UD1 = 4		# 1   # Double height format (should be 1 )
+    DHD = 1		# 0/1 # 1: display shift enable  0: dot scroll enable
+
+    S   = 1		# display shift enable
+
+    SC  = 8		# 1: Scroll  0: cursor shift
+    RL  = 4		# Scroll(shift) direction 1: right  0: left
+    #---------------------------------------------------------------------------------
+
+    # # INTERNAL methods # #
+
+    @staticmethod
+    def ms_delay(n):
+	time.sleep(n / 1000000.0)
+
+    @staticmethod
+    def so_cmd(c) :
+	ld._i2c.write_byte_data(ld.i2cAddr_LD, 0, c)
+	ld.ms_delay(5)
+
+    @staticmethod
+    def init_1602() :
+
+	ld.DH  = 0
+	ld.DHD = 1
+	ld.clear_display()
+	ld.ms_delay(20)
+
+    # # PUBLIC METHODS # #
+
+    @staticmethod
+    def init(i2c):
+	ld._i2c = i2c
+	ld.init_1602()
+
+    @staticmethod
+    def clear_display() :
+	ld.so_cmd(0x01)
+
+    @staticmethod
+    def display_sw(sw):
+	if sw == 1:
+	    ld.so_cmd(0x0c)
+	else:
+	    ld.so_cmd(0x08)
+
+#====================================================================#
+#  heat source detector class
+#====================================================================#
+
+class hsd:
+
+    _mode = 0			# 0:mode off , 1: hsd mode
+    _is_someone = 0
+    _dtect_count = 0		# detection signal count in a minute
+    _t_detect = None		# the time of sensor detect heat source(=null when _is_someone == false)
+    _t_confirming = None	# when the sensor detect twice within 3.0 second, it's regard as "detection". this store the first detection time.
+    _detect_count = 0
+
+    @staticmethod
+    def init():
+
+	GPIO.setmode( GPIO.BCM )
+	GPIO.setup( 10, GPIO.IN )
+	GPIO.add_event_detect( 10, GPIO.RISING, callback = hsd.hsd_callback )
+
+	hsd._t_confirming = time.time()
+
+	if hsd._mode == 0 :
+	    hsd._is_someone= 1
+	else :
+	    hsd._is_someone= 0
+
+	return
+
+    #
+    #  set configuration
+    #
+    @staticmethod
+    def set_mode(new_mode) :
+	logger = logging.getLogger(__name__)
+
+	if hsd._mode == new_mode:
+	    return
+
+	hsd._mode = new_mode
+	if hsd._mode== 0 :
+	    logger.info("set_mode:_is_someone = 1")
+	    hsd._is_someone = 1
+	else :
+	    logger.info("set_mode:_is_someone = 0")
+	    hsd._is_someone = 0
+
+    #
+    # hsd callback
+    #
+    #   update _t_detect by current time
+    #
+    @staticmethod
+    def hsd_callback(portNo):
+
+	logger = logging.getLogger(__name__)
+	hsd._t_detect = time.time()
+	logger.debug("hsd triggered")
+	hsd._detect_count += 1
+
+	return
+
+    @staticmethod
+    def get_detect_count():
+
+	return_val = hsd._detect_count
+	hsd._detect_count = 0
+	return return_val
+        
+    #
+    #   expected to be called periodically
+    #
+    #    decide _is_someone or not
+    #
+    @staticmethod
+    def polling():
+
+	logger = logging.getLogger(__name__)
+	if hsd._mode == 1 and hsd._t_detect != None :
+
+	    if hsd._is_someone :
+		# acquired
+		if time.time() - hsd._t_detect > 10 :
+		    hsd._is_someone = 0
+		    hsd._t_detect = None
+		    logger.debug("hsd: leaved")
+
+	    elif (hsd._t_confirming == None) or (hsd._t_detect - hsd._t_confirming > 3.0) :
+		# first event
+		hsd._t_confirming = hsd._t_detect
+		hsd._t_detect = None
+		logger.debug("hsd: confirming...")
+
+	    else :
+		# subsequent event(now acquireing)
+		hsd._t_confirming = None
+		hsd._is_someone = 1
+		logger.debug("hsd: detect")
+
+	return
+
+#====================================================================#
+#  message acceptor (from child process)
+#====================================================================#
+
+class m_a:
+
+    _hsd_mode = 2
+    _t_received = 0
+    _fifo = '/tmp/pipe'
+    _led_current = 0
+
+    @staticmethod
+    def init():
+	logger = logging.getLogger(__name__)
+
+	m_a._t_received = time.time()
+	try:
+	    os.mkfifo(m_a._fifo)
+	except EnvironmentError as ee:
+	    logger.info("fifo cannot create:" + str(ee.errno))
+	    if ee.errno != errno.EEXIST:
+		logger.error("fifo cnnot create" + ee.args)
+		raise
+
+	th = threading.Thread(target = m_a.fifo_listner)
+	th.setDaemon(True)
+	th.start()
+
+    @staticmethod
+    def fifo_listner():
+	logger = logging.getLogger(__name__)
+
+	while True:
+	    logger.debug("waiting fifo...");
+	    with open (m_a._fifo) as f:
+		data = f.read()
+		if len(data) == 0 : break
+		m_a._hsd_mode = int(data)
+		m_a._t_received = time.time()
+		logger.debug("data received from fifo " + str(m_a._hsd_mode))
+		hsd.set_mode(m_a._hsd_mode)
+
+    @staticmethod
+    def polling():
+	logger = logging.getLogger(__name__)
+
+	if m_a._hsd_mode == 2:
+	    return
+
+	if time.time() - m_a._t_received > 125:
+	    if m_a._hsd_mode != 2:
+		logger.warning("fifo timeout... hsd is closed")
+		m_a._hsd_mode = 2
+		m_a._led_current = 0
+		logger.debug("led OFF");
+		ld.display_sw(0)
+	else:
+	    if m_a._led_current != hsd._is_someone :
+		m_a._led_current = hsd._is_someone
+		if m_a._led_current == 1 :
+		    logger.debug("led ON");
+		    ld.display_sw(1)
+		else:
+		    logger.debug("led OFF");
+		    ld.display_sw(0)
+
+#====================================================================#
+#  update sens data 
+#====================================================================#
+
+#
+#   Get CPU temperature
+#
+def get_cpu_thermal():
+    f = open('/sys/class/thermal/thermal_zone0/temp','r')
+    temp_c = long(f.read()) / 1000.0
+    f.close()
+
+    return temp_c
+
+
+#
+#   SHT-31 sensor read
+#
+i2cAddr_SHT31 = 0x45
+
+def measure_T_H():
+
+    global __i2c
+
+    logger = logging.getLogger(__name__)
+    logger.debug("read from sensor device...")
+    __i2c.write_i2c_block_data(i2cAddr_SHT31, 0x2c, [0x06])
+    time.sleep(0.02)
+    data = __i2c.read_i2c_block_data(i2cAddr_SHT31, 0, 6)
+
+    temp_s   = ( 175 * (data[0] * 256 + data[1]) / 65535.0) - 45
+    humidity =   100 * (data[3] * 256 + data[4]) / 65535.0
+
+#    logger.debug(str(temp_s) + ":" + str(humidity))
+    logger.debug("{:05.2f}C {:04.1f}%".format(temp_s, humidity))
+
+    return temp_s, humidity
+
+#
+#   POST to GAE
+#
+
+def postToGAE(temp_s, temp_c, humidity, sensCount):
+
+    logger = logging.getLogger(__name__)
+
+    postUrl = "https://jebaxxmonitor.appspot.com/postData"
+    params = { 
+	"sensData[%s]" % "timestamp" : datetime.now().isoformat(),
+	"sensData[%s]" % "T-SHT-31"  : temp_s ,
+	"sensData[%s]" % "H-SHT-31"  : humidity ,
+	"sensData[%s]" % "C-HC501"   : sensCount ,
+	"sensData[%s]" % "T-cpu_emily" : temp_c }
+
+    try:
+	response = requests.post(postUrl, data = params)
+    except EnvironmentError:
+	logger.warning("connection error 1")
+	sleep(5)
+	try:
+	    response = requests.post(postUrl, data = params)
+	except EnvironmentError:
+	    logger.warning("connection error 2")
+	    pass
+
+    logger.debug("response.code = %d" % response.status_code)
+
+#
+#   write sensed data to the data file and the server
+#
+def sens_and_record(post_flag = 0):
+
+    (temp_s, humidity) = measure_T_H()
+    temp_c = get_cpu_thermal()
+    detect_count = hsd.get_detect_count()
+
+    with open('/tmp/sens_data.txt', 'w') as f:
+	f.write(str(temp_s)+','+ str(temp_c) + ',' + str(humidity))
+
+    if post_flag :
+	postToGAE(temp_s, temp_c, humidity, detect_count)
+
+######===============================================================#
+#
+#  Main Module
+#
+######===============================================================#
+
+logging.basicConfig(format='%(asctime)s %(funcName)s %(message)s', filename='/tmp/p2.log',level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+#
+__i2c = smbus.SMBus(1)
+ld.init(__i2c)
+
+hsd.init()
+m_a.init()
+c3_m.init(__i2c)
+#
+sens_and_record(post_flag = 0)
+
+m_time = (time.time() // 60)
+
+try:
+    while 1:
+	time.sleep(0.25)
+	hsd.polling()
+	m_a.polling()
+	c3_m.polling()
+
+	if (m_time != (time.time()) // 60):
+	    m_time = (time.time()) // 60
+
+	    sens_and_record(post_flag = 1)
+
+except KeyboardInterrupt:
+
+    ld.display_sw(0)
+    GPIO.cleanup()
+
---
+++ monitorUI.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python
+#
+import sys
+import logging
+import time
+import RPi.GPIO as GPIO
+import smbus
+from datetime import datetime
+from shutil import copyfile
+
+__i2c = None
+
+######################################################################
+#  Sub process (cooperate with monitorBase)
+#
+#	manage LED display and button devices
+#====================================================================#
+
+################################################
+# LED display library
+
+class ld():
+
+    i2cAddr_LD = 0x3c	# SO1602 LED display I2C Address
+    _i2c = None
+
+    #---------------------------------------------------------------------------------
+    N   = 8		# 1   # 1: 2/4-line mode  0: 1/3-line mode
+    DH  = 4		# 0/1 # Double height font control for 2-line mode
+    RE  = 2		# ... # RE flag
+    IS  = 1		# ... # IS flag
+    BE  = 4		# 0   # CGRAM blink 1: Enable  0: Disable
+    REV = 1		# 0   # 1: Reverse display  0: Normal display
+
+    FW  = 4		# 0   # 1: 6-dot font  0: 5-dot font
+    BW  = 2		# 0   # inverting cursor  1: Enable 0: disable
+    NW  = 1		# 0   # 1: 3 or 4 line  0: 1 or 2 line
+    #---------------------------------------------------------------------------------
+    UD2 = 8		# 1   # Double height format (should be 1 )
+    UD1 = 4		# 1   # Double height format (should be 1 )
+    DHD = 1		# 0/1 # 1: display shift enable  0: dot scroll enable
+
+    S   = 1		# display shift enable
+
+    SC  = 8		# 1: Scroll  0: cursor shift
+    RL  = 4		# Scroll(shift) direction 1: right  0: left
+    #---------------------------------------------------------------------------------
+
+    # # INTERNAL methods # #
+
+    @staticmethod
+    def ms_delay(n):
+	time.sleep(n / 1000000.0)
+
+    @staticmethod
+    def so_cmd(c) :
+	ld._i2c.write_byte_data(ld.i2cAddr_LD, 0, c)
+	ld.ms_delay(5)
+
+    @staticmethod
+    def set_Line_mode_1_2() :
+        
+	ld.so_cmd(0x20 | (ld.N  & ld.N) | (ld.BE & 0) | (ld.RE & ld.RE) | (ld.REV & 0))	# RE = 1 , N = 1 : 2 or 4 line
+	ld.so_cmd(0x08 | (ld.FW & 0) | (ld.BW & 0) | (ld.NW & 0))		        # NW = 0 :  1 or 2 line
+	ld.so_cmd(0x20 | (ld.N  & ld.N) | (ld.DH)     | (ld.RE &  0) | (ld.IS &  0))	# RE = 0 , n = 1 : 2 or 4 line
+
+    @staticmethod
+    def init_1602() :
+
+	ld.DH  = 0
+	ld.DHD = 1
+	ld.set_Line_mode_1_2()
+	ld.clear_display()
+	ld.ms_delay(20)
+	ld.return_to_home()
+	ld.display_sw(1)
+	ld.return_to_home()
+	ld.ms_delay(20)
+
+    # # PUBLIC METHODS # #
+
+    @staticmethod
+    def init(i2c):
+	ld._i2c = i2c
+	ld.init_1602()
+	ld.set_contrast(0x38)
+	ld.clear_display()
+
+    @staticmethod
+    def clear_display() :
+	ld.so_cmd(0x01)
+
+    @staticmethod
+    def return_to_home():
+	ld.so_cmd(2)
+
+    @staticmethod
+    def display_sw(sw):
+	if sw == 1:
+	    ld.so_cmd(0x0c)
+	else:
+	    ld.so_cmd(0x08)
+
+    @staticmethod
+    def set_double_height(sw):
+	if sw == 0:	ld.DH = 0
+	else:		ld.DH = 4
+	ld.so_cmd(0x20 | (ld.N  & ld.N) | (ld.DH)     | (ld.RE &  0) | (ld.IS &  0))	# RE = 0 , n = 1 : 2 or 4 line
+
+    @staticmethod
+    def set_contrast(c) :
+
+	ld.so_cmd(0x20 | (ld.N  & ld.N) | (ld.BE & 0) | (ld.RE & ld.RE) | (ld.REV & 0))		# RE = 1
+	ld.so_cmd(0x79)										# SD = 1
+	ld.so_cmd(0x81)										# set contrast
+	ld.so_cmd(c)										# value
+	ld.so_cmd(0x78)										# SD = 0
+	ld.so_cmd(0x20 | (ld.N  & ld.N) | (ld.DH)     | (ld.RE &  0) | (ld.IS & 0))		# RE = 0
+
+    @staticmethod
+    def set_location(l, c):
+	c = c + 0x80        # 1st line is start with 0x80
+	if l == 1:
+	    c = c + 0x40    # 2nd line is start with 0xc0
+        
+	ld.so_cmd(c)
+
+    @staticmethod
+    def write_char(str, l = None, c = None):
+        
+	if (c != None) :
+	    ld.set_location(l, c)
+
+	ld._i2c.write_i2c_block_data(ld.i2cAddr_LD, 0x40, map(ord, str))
+
+######################################################################
+#  display manager class
+
+class d_m():
+
+    _i2c = None
+    _state = ""
+    _states = [ 'config', 'clock', 'sensor' ]
+
+    I2CADDR_BTN  = 0x3f  # button shim I2C Address
+    _key_state   = 0
+    _current_sw  = 0
+
+    REG_INPUT    = 0x00
+    REG_OUTPUT   = 0x01
+    REG_POLARITY = 0x02
+    REG_CONFIG   = 0x03
+
+    NUM_BUTTONS = 5
+
+    _clock_form = [	'%m/%d %H:%M %a',
+			' %m/%d %H:%M %a',
+			'%m/%d %H:%M',
+			'  %m/%d %H:%M',
+			'    %m/%d %H:%M',
+			'%H:%M  %a',
+			'  %H:%M  %a',
+			'    %H:%M %a',
+			'      %H:%M %a',
+			'        %H:%M',
+			'          %H:%M' ]
+    _c_form_no = 0
+
+    _sens_form = [	'{temp:05.2f}C   {hum:04.1f}%' ,
+			' {temp:05.2f}C   {hum:04.1f}%' ,
+			'  {temp:05.2f}C  {hum:04.1f}%' ]
+
+    _s_form_no = 0
+    _sc_form_no = 0
+
+    @staticmethod
+    def init(__i2c) :
+	logger = logging.getLogger(__name__)
+	d_m._i2c = __i2c
+	d_m._state = c_m.get('initial_dm_state')
+	GPIO.setmode( GPIO.BCM )
+	GPIO.setup( 27, GPIO.IN )
+	GPIO.add_event_detect( 27, GPIO.FALLING, callback = d_m.button_callback )
+	ld.init(__i2c)
+	d_m.redraw_display()
+
+    #
+    #  set configuration
+    #
+    @staticmethod
+    def set_initial_state(new_state) :
+	d_m._state = new_state
+
+    #
+    #   expected to be called periodically
+    #
+    @staticmethod
+    def polling() :
+	pass
+
+    #
+    #  button_callback
+    #
+    #   get button state(whitch button has been pushed)
+    #
+    @staticmethod
+    def button_callback(portNo):
+        
+	logger = logging.getLogger(__name__)
+	key_state = d_m._i2c.read_byte_data(d_m.I2CADDR_BTN, d_m.REG_INPUT)
+	key_state = ~key_state & 0b011111
+	logger.debug("key_detect:" + str(key_state))
+
+	if (key_state & 0b000001) > 0 :
+	    #
+	    # change _state
+	    #
+	    d_m.change_state()
+
+	if key_state & 0b011110 :
+	    if d_m._state == 'clock' or d_m._state == 'sensor' :
+		d_m.key_event(key_state)
+	    elif d_m._state == 'config':
+		c_m.key_event(key_state)
+
+    #
+    #  Change d_m_status
+    #
+    @staticmethod
+    def change_state():
+
+	i = d_m._states.index(d_m._state) + 1
+	if i == len(d_m._states): i = 0
+	d_m._state = d_m._states[i]
+
+	ld.clear_display()
+
+	if d_m._state == 'config':
+	    c_m.redraw_display()
+	else:
+	    d_m.redraw_display()
+
+    #
+    #  key event handler
+    #
+    @staticmethod
+    def key_event(key_state) :
+
+	logger = logging.getLogger(__name__)
+	if d_m._state == 'clock':
+	    if key_state & 0b001010 :
+		d_m._c_form_no += 1
+		if d_m._c_form_no == len(d_m._clock_form): d_m._c_form_no = 0
+	    elif key_state & 0b010100 :
+		d_m._c_form_no -= 1
+		if d_m._c_form_no == -1: d_m._c_form_no = len(d_m._clock_form) - 1
+
+	    ld.clear_display()
+	    d_m.redraw_display()
+
+	elif d_m._state == 'sensor':
+	    if key_state & 0b000010 :
+		d_m._sc_form_no += 1
+		if d_m._sc_form_no == len(d_m._clock_form)+1 : d_m._sc_form_no = 0
+	    elif key_state & 0b000100 :
+		d_m._sc_form_no -= 1
+		if d_m._sc_form_no == -1 : d_m._sc_form_no = len(d_m._clock_form)
+	    elif key_state & 0b001000 :
+		d_m._s_form_no += 1
+		if d_m._s_form_no == len(d_m._sens_form) : d_m._s_form_no = 0
+	    elif key_state & 0b010000 :
+		d_m._s_form_no -= 1
+		if d_m._s_form_no == -1 : d_m._s_form_no = len(d_m._sens_form) - 1
+
+	d_m.redraw_display()
+
+    #
+    #  redraw display
+    #
+    @staticmethod
+    def redraw_display():
+
+	ld.clear_display()
+	if d_m._state == 'clock' or d_m._sc_form_no == 0 : ld.set_double_height(1)
+	else			: ld.set_double_height(0)
+
+	d_m.refresh_display()
+
+    @staticmethod
+    def read_sens_data():
+
+	try:
+	    with open('/tmp/sens_data.txt', 'r') as f:
+		return [float(d) for d in f.read().split(',')]
+	except:
+	    copyfile('/tmp/sens_data.txt', '/tmp/sens_data_err.txt')
+	    raise
+
+    #
+    #  (periodical) refresh display
+    #
+    @staticmethod
+    def refresh_display():
+
+	if d_m._state == 'clock':
+	    ld.write_char(datetime.now().strftime(d_m._clock_form[d_m._c_form_no]), 0, 0)
+	elif d_m._state == 'sensor':
+	    temp, temp_c, hum = d_m.read_sens_data()
+	    if d_m._sc_form_no != 0 :
+		ld.write_char(datetime.now().strftime(d_m._clock_form[d_m._sc_form_no-1]), 0, 0)
+		ld.write_char(d_m._sens_form[d_m._s_form_no].format(temp=temp, hum=hum), 1, 0)
+	    else :
+		ld.write_char(d_m._sens_form[d_m._s_form_no].format(temp=temp, hum=hum), 0, 0)
+	elif d_m._state == 'config':
+	    c_m.refresh_display()
+	else:
+	    ld.write_char('unknown', 0, 0)
+
+######################################################################
+#  config manager class
+
+class c_m:
+        
+    _conf = {}
+    _conf['initial_dm_state'] = 'clock'
+    _conf['hsd_mode'] = 1
+    
+    @staticmethod
+    def get(conf_name):
+
+	return c_m._conf[conf_name]
+
+    @staticmethod
+    def redraw_display():
+
+	ld.set_double_height(1)
+	c_m.refresh_display()
+
+    @staticmethod
+    def refresh_display():
+
+	ld.write_char('config', 0, 0)
+
+    @staticmethod
+    def key_event(key_state):
+
+	pass
+
+######################################################################
+#  MAIN
+
+logging.basicConfig(format='%(asctime)s %(funcName)s %(message)s', filename='/tmp/p2.log',level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+
+__i2c = smbus.SMBus(1)
+ld.init(__i2c)
+
+d_m.init(__i2c)
+m_time = (time.time() // 60)
+
+hsd_mode = c_m.get('hsd_mode')
+time.sleep(5)
+try:
+    with open('/tmp/pipe', 'w') as f:
+	f.write(str(hsd_mode))
+except EnvironmentError:
+    logger.error("pipe cannot open")
+    sys.exit()
+
+try:
+    while 1:
+	time.sleep(0.25)
+	d_m.polling()
+
+	if (m_time != (time.time()) // 60):
+	    m_time = (time.time()) // 60
+
+	    logger.debug("Do refresh Display")
+	    time.sleep(0.05)
+	    d_m.refresh_display()
+	    try:
+		with open('/tmp/pipe', 'w') as f:
+		    f.write(str(hsd_mode))
+	    except EnvironmentError:
+		logger.error("pipe cannot open")
+		sys.exit()
+
+except KeyboardInterrupt:
+
+    GPIO.cleanup()
+
